5cc0fca66c4b35c6b3e4e249696c1f472ccd84ce
Jeremy R <24259309+JRSQ1231@users.noreply.github.com>
2019-03-11 18:02:38 -0400

    exercise 7 and 8 added

---
+++ LPTHW/ex7.py
@@ -0,0 +1,27 @@
+#  prints the statement in quotes
+print("Mary had a little lamb.")
+#  prints the statement in quotes, and formats the {} as the string "snow".
+print("Its fleece was white as {}.".format('snow'))
+#  prints the statement in quotes.
+print("And everywhere that Mary went.")
+#  prints 10 periods.
+print("." * 10)  # what'd that do?
+
+#  Assigns string etters to each noted variable.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#  Watch end = ' ' at the end.  Try removing it and see what happens
+#  Concatenates all of the "end" variables to spell out cheese burger.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
