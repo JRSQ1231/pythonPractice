ab055c1836a31708d124986b95c7a3f3c1e28e71
Jeremy R <24259309+JRSQ1231@users.noreply.github.com>
2019-03-18 16:50:25 -0400

    ex10

---
+++ LPTHW/ex10.py
@@ -0,0 +1,19 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+\t* Testing what the \a does
+Now what the \f does
+and what the \bdoes
+as well as the \v, what does this do?
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
