72a66f67d01d1f9b4c4eb8860b7e2c0cb6963cef
Jeremy R <24259309+JRSQ1231@users.noreply.github.com>
2020-04-07 17:39:22 -0400

    added Pluralsight work

---
+++ Pluralsight/functions.py
@@ -0,0 +1,22 @@
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for students in students:
+        students_titlecase = students.title()
+    return students_titlecase
+
+
+def print_students_titlecase():
+    students_titlecase = []
+    for students in students:
+        students_titlecase = student.title()
+print(students_titlecase)
+
+
+def add_student(name):
+    students.append(name)
+
+
+student_list = get_students_titlecase()
