ff6187fc87cb01fd1dae46ec0f9895008a105dba
Jeremy R <24259309+JRSQ1231@users.noreply.github.com>
2020-05-17 16:03:52 -0400

    commit

--- Pluralsight/functions.py
+++ Pluralsight/functions.py
@@ -3,20 +3,34 @@ students = []
 
 def get_students_titlecase():
     students_titlecase = []
-    for students in students:
-        students_titlecase = students.title()
+    for student in students:
+        students_titlecase = student.title()
     return students_titlecase
 
 
-def print_students_titlecase():
+'''def print_students_titlecase():
     students_titlecase = []
     for students in students:
         students_titlecase = students.title()
-print(students_titlecase)
+    print(students_titlecase) '''
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
+
+ 
+def add_student(name, student_id=332):
+    student = {"name": name, "student_id": student_id }
+    students.append(student)
 
 
-def add_student(name):
-    students.append(name)
+def var_args(name, *args):
+    print(name)
+    print(args)
 
 
 student_list = get_students_titlecase()
+
+add_student(name="Mark", student_id=15)
+
+var_args("Mark", "loves Python", None, "Hello", True)
